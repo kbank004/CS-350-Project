52e0bb697ff7d29948b72b59c16130312364b08c
takky1154 <>
2021-10-31 18:16:03 -0400

    Add manifest attribute

--- src/main/java/edu/odu/cs/cs350/DupDetector/DupDetector.java
+++ src/main/java/edu/odu/cs/cs350/DupDetector/DupDetector.java
@@ -1,7 +1,7 @@
 package edu.odu.cs.cs350.DupDetector;
 
 public class DupDetector {
-    public static void Main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Hello World");
     }
 }
