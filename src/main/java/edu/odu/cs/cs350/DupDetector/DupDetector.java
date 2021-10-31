93449ae99273505ea6bd068e1b0e5ed1e03faa7c
takky1154 <>
2021-10-31 17:04:06 -0400

    Set up Gradle project and file structure

---
+++ src/main/java/edu/odu/cs/cs350/DupDetector/DupDetector.java
@@ -0,0 +1,7 @@
+package edu.odu.cs.cs350.DupDetector;
+
+public class DupDetector {
+    public static void Main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/test/java/edu/odu/cs/cs350/DupDetector/DupDetectorTest.java
@@ -0,0 +1,7 @@
+package edu.odu.cs.cs350.DupDetector;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DupDetectorTest {
+    
+}
