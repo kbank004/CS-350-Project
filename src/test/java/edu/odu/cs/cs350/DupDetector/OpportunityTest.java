0cf77f8352c88f032e75e0b43ba86109d5a472ae
Spencer Hite <44657053+spencerhite@users.noreply.github.com>
2021-12-04 20:59:43 -0500

    initiated OpportunityTest.java

---
+++ src/test/java/edu/odu/cs/cs350/DupDetector/OpportunityTest.java
@@ -0,0 +1,15 @@
+package edu.odu.cs.cs350.DupDetector;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.nio.file.Path;
+
+public class OpportunityTest {
+	
+}
