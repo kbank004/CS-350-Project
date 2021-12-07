7e5161622d2742330f3cb86fbcf3f2e8402a3030
Gabriel Lugo <g.alexander.lugo@gmail.com>
2021-12-07 01:54:39 -0500

    Update TokenType.java

--- src/main/java/edu/odu/cs/cs350/DupDetector/TokenType.java
+++ src/main/java/edu/odu/cs/cs350/DupDetector/TokenType.java
@@ -10,16 +10,22 @@ package edu.odu.cs.cs350.DupDetector;
  * 
  */
 public enum TokenType {
-  IDENTIFIER,
-  KEYWORD,
-  SYMBOL,
-  INTEGER_LIT,
-  FLOAT_LIT,
-  STRING_LIT,
-  CHAR_LIT,
-  BOOL_LIT,
+  IDENTIFIER(0),
+  KEYWORD(1),
+  SYMBOL(2),
+  INTEGER_LIT(3),
+  FLOAT_LIT(4),
+  STRING_LIT(5),
+  CHAR_LIT(6),
+  BOOL_LIT(7),
   // COMMENT, // ignore
   // PREPROCESSOR, // ignore
   // WHITE_SPACE, // ignore
-  EOF
+  EOF(-1);
+
+  public final int value;
+
+  private TokenType(int value_) {
+    value = value_;
+  }
 }
